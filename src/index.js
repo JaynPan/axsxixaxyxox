9ae2d0d1a951654f47af83e97e366f8caebd0dc2
Jay Pan <lb0104672@gmail.com>
2022-08-06 23:20:12 +0800

    config webpack5 + babel7 + react17

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  color: red;
+`
+
+const HelloWorld = () => {
+    return (
+      <H1>
+          Hello World 1
+      </H1>
+    );
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
