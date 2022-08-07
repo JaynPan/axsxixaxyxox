cefedd68f81f363a81594aaece47534777ca4e9f
Jay Pan <lb0104672@gmail.com>
2022-08-07 11:29:12 +0800

    config ESLint

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,10 @@ import styled from 'styled-components';
 
 const H1 = styled.h1`
   color: red;
-`
+`;
 
 const HelloWorld = () => {
-    return (
-      <H1>
-          Hello World
-      </H1>
-    );
-}
+  return <H1>Hello World</H1>;
+};
 
-ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.join(__dirname, "src", "index.tsx"),
-    output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
-    mode: process.env.NODE_ENV || "development",
-    resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
-    devServer: { static: { directory: path.join(__dirname, "src") } },
-    module: {
-      rules: [
-          { 
-              test: /\.(js|jsx)$/, 
-              exclude: /node_modules/, 
-              use: ["babel-loader"] 
-          },
-          {
-            test: /\.(ts|tsx)$/,
-            exclude: /node_modules/,
-            use: ["ts-loader"],
-          },
-          { 
-              test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-              use: ["file-loader"] 
-          },
-      ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "src", "index.html"),
-        }),
+  entry: path.join(__dirname, 'src', 'index.tsx'),
+  output: { path: path.join(__dirname, 'build'), filename: 'index.bundle.js' },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  devServer: { static: { directory: path.join(__dirname, 'src') } },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ['file-loader'],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+  ],
 };
